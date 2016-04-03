a9da4dd9dbb7ca0f20652fa13f8d7a8086b3e148
nick0kcin <nikkholod@mail.ru>
2016-04-03 21:19:55 +0300

    Update FindRoads.h

--- FindRoads.h
+++ FindRoads.h
@@ -69,43 +69,43 @@ void bfs(Mat* im,unsigned char* col,Point p0,Mat* out,double (*dist)(unsigned ch
 Mat FindRoads(Mat& map,double coeff1=800,double coeff2=850)
 {
 	Mat maplab;
-	cvtColor(map,maplab,CV_RGB2Lab);// перевод в цветовую схему лаб,чтобы оценивать цветовое отличие
+	cvtColor(map,maplab,CV_RGB2Lab);// РїРµСЂРµРІРѕРґ РІ С†РІРµС‚РѕРІСѓСЋ СЃС…РµРјСѓ Р»Р°Р±,С‡С‚РѕР±С‹ РѕС†РµРЅРёРІР°С‚СЊ С†РІРµС‚РѕРІРѕРµ РѕС‚Р»РёС‡РёРµ
 	Mat out(map.rows,map.cols,CV_8UC1);
 	Mat out2(map.rows,map.cols,CV_8UC1);
 	out.setTo(0);
 	out2.setTo(0);
 	for(int i=0;i<map.rows;i++)
 		for(int j=0;j<map.cols;j++)
-			if(Euclid3Square(At((&maplab),i,j),roadpixel)<coeff1)   //выбираем пиксели -кандидаты на то,чтобы быть дорогой
+			if(Euclid3Square(At((&maplab),i,j),roadpixel)<coeff1)   //РІС‹Р±РёСЂР°РµРј РїРёРєСЃРµР»Рё -РєР°РЅРґРёРґР°С‚С‹ РЅР° С‚Рѕ,С‡С‚РѕР±С‹ Р±С‹С‚СЊ РґРѕСЂРѕРіРѕР№
 				*At((&out),i,j)=255;
-	erode(out,out,getStructuringElement(cv::MorphShapes::MORPH_RECT,Size(3,3)),Point(-1,-1),2); // подавляем малые группы пикселей
+	erode(out,out,getStructuringElement(cv::MorphShapes::MORPH_RECT,Size(3,3)),Point(-1,-1),2); // РїРѕРґР°РІР»СЏРµРј РјР°Р»С‹Рµ РіСЂСѓРїРїС‹ РїРёРєСЃРµР»РµР№
 	//dilate(out,out,getStructuringElement(cv::MorphShapes::MORPH_RECT,Size(3,3)),Point(-1,-1),1);
 	unsigned char val=25;
 	for(int i=0;i<map.rows;i++)
 		for(int j=0;j<map.cols;j++)
-			if(*At((&out),i,j)==255)                                                      // из каждого кандидата запускаем поиск в ширину
-				bfs(&maplab,At((&maplab),i,j),Point(i,j),&out2,Euclid3Square,coeff2,val); // те пиксели,куда смогли добраться -дорога
-	                                                                                      // точки до которыхдобрались из разных начальных помечаем разным цветом
+			if(*At((&out),i,j)==255)                                                      // РёР· РєР°Р¶РґРѕРіРѕ РєР°РЅРґРёРґР°С‚Р° Р·Р°РїСѓСЃРєР°РµРј РїРѕРёСЃРє РІ С€РёСЂРёРЅСѓ
+				bfs(&maplab,At((&maplab),i,j),Point(i,j),&out2,Euclid3Square,coeff2,val); // С‚Рµ РїРёРєСЃРµР»Рё,РєСѓРґР° СЃРјРѕРіР»Рё РґРѕР±СЂР°С‚СЊСЃСЏ -РґРѕСЂРѕРіР°
+	                                                                                      // С‚РѕС‡РєРё РґРѕ РєРѕС‚РѕСЂС‹С…РґРѕР±СЂР°Р»РёСЃСЊ РёР· СЂР°Р·РЅС‹С… РЅР°С‡Р°Р»СЊРЅС‹С… РїРѕРјРµС‡Р°РµРј СЂР°Р·РЅС‹Рј С†РІРµС‚РѕРј
 
-			   std::set<char> neibours[256];   //находим области,которые целиком внутри других областей
+			   std::set<char> neibours[256];   //РЅР°С…РѕРґРёРј РѕР±Р»Р°СЃС‚Рё,РєРѕС‚РѕСЂС‹Рµ С†РµР»РёРєРѕРј РІРЅСѓС‚СЂРё РґСЂСѓРіРёС… РѕР±Р»Р°СЃС‚РµР№
 		   for(int i=0;i<map.rows;i++)
 		   for(int j=0;j<map.cols;j++)
 		   {
 			   	 for(int x=-1;x<=1;x++)
 				   for(int y=-1;y<=1;y++)
-			   if(((x==0)^(y==0))&&i+x>=0&&j+y>=0&&i+x<map.rows&&j+y<map.cols&&*At((&out2),i+x,j+y)!=*At((&out2),i,j))//находим соседей в 4связной области
+			   if(((x==0)^(y==0))&&i+x>=0&&j+y>=0&&i+x<map.rows&&j+y<map.cols&&*At((&out2),i+x,j+y)!=*At((&out2),i,j))//РЅР°С…РѕРґРёРј СЃРѕСЃРµРґРµР№ РІ 4СЃРІСЏР·РЅРѕР№ РѕР±Р»Р°СЃС‚Рё
 				   neibours[*At((&out2),i,j)].insert(*At((&out2),i+x,j+y));
 		   }
 		   for(int i=0;i<map.rows;i++)
 		       for(int j=0;j<map.cols;j++)
-				   if(neibours[*At((&out2),i,j)].size()==1&&*neibours[*At((&out2),i,j)].begin()!=0) //объединяем их с соответстыующими внешними
+				   if(neibours[*At((&out2),i,j)].size()==1&&*neibours[*At((&out2),i,j)].begin()!=0) //РѕР±СЉРµРґРёРЅСЏРµРј РёС… СЃ СЃРѕРѕС‚РІРµС‚СЃС‚С‹СѓСЋС‰РёРјРё РІРЅРµС€РЅРёРјРё
 			   {
 				   *At((&out2),i,j)=*neibours[*At((&out2),i,j)].begin();
 			   }
 
 
 		for(int i=0;i<map.rows;i++)
-		   for(int j=0;j<map.cols;j++) //выделяем границы
+		   for(int j=0;j<map.cols;j++) //РІС‹РґРµР»СЏРµРј РіСЂР°РЅРёС†С‹
 		   {
 			   bool ex=0;
 			   for(int x=-1;x<=1;x++)
@@ -115,12 +115,12 @@ Mat FindRoads(Mat& map,double coeff1=800,double coeff2=850)
 			   if(ex)
 				   (*At((&out2),i,j))++;
 		   }
-			    	for(int i=0;i<map.rows;i++)//удаляем внутрености
+			    	for(int i=0;i<map.rows;i++)//СѓРґР°Р»СЏРµРј РІРЅСѓС‚СЂРµРЅРѕСЃС‚Рё
 		   for(int j=0;j<map.cols;j++)
 			   if(*At((&out2),i,j)%5!=1)
 				   *At((&out2),i,j)=0;
 
-		for(int i=0;i<map.rows;i++)//заполняе белым цветом
+		for(int i=0;i<map.rows;i++)//Р·Р°РїРѕР»РЅСЏРµ Р±РµР»С‹Рј С†РІРµС‚РѕРј
 		   for(int j=0;j<map.cols;j++)
 			   if(*At((&out2),i,j)>1)
 			   {
